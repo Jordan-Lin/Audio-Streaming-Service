6f946189d02e0ab82b777d2366bbb967628c301d
Jordan Lin <jordan.lin09@gmail.com>
2017-04-12 09:20:10 -0700

    hid debug window, change file path for release

--- songmanager.cpp
+++ songmanager.cpp
@@ -45,21 +45,20 @@ void SongManager::unlockedSendSongList() {
 
 void SongManager::LoadSongList()
 {
-    QString testAudioFilePath = "C:\\Users\\Administrator\\Desktop\\CommAudioWavFiles";                    // Test, change to directory next to app
-
-    DebugWindow::get()->logd(testAudioFilePath);
-    QDir testAudioDirectory;                                                                    // Test, change to directory next to app
-    testAudioDirectory.setPath(testAudioFilePath);
+    QString songdirectory = QDir::currentPath() + QString("/Songs");
+    DebugWindow::get()->logd(songdirectory);
+    QDir audioDirectory;
+    audioDirectory.setPath(songdirectory);
     DebugWindow::get()->logd("Setting list filters");
     QStringList filters;
     filters << "*.wav";
-    testAudioDirectory.setNameFilters(filters);
-    testAudioDirectory.setSorting(QDir::Name);
+    audioDirectory.setNameFilters(filters);
+    audioDirectory.setSorting(QDir::Name);
 
     DebugWindow::get()->logd("Getting entryList");
-    QStringList files = testAudioDirectory.entryList();
+    QStringList files = audioDirectory.entryList();
     for (const QString& f : files) {
-        Song temp(SongManager::get().genId(), f, "Unknown", "Uknown", testAudioFilePath + "\\" + f);
+        Song temp(SongManager::get().genId(), f, "Unknown", "Uknown", songdirectory + "\\" + f);
         SongManager::get().addSong(temp);
     }
 }
