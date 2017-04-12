21c97b42e6ec2a484e608c4a6636b80ca0c5c202
Trista Huang <trista.huang@outlook.com>
2017-04-12 04:28:48 -0700

    more code headers added

--- songstreamreceiver.cpp
+++ songstreamreceiver.cpp
@@ -1,9 +1,47 @@
+/*------------------------------------------------------------------------------
+-- SOURCE FILE: songstreamereceiver.cpp
+--
+-- PROGRAM: CommAudio
+--
+-- FUNCTIONS:
+--  SongStreamReceiver(int listenPort);
+    void init();
+    static void CALLBACK receiveSongStreamRoutine(DWORD err, DWORD bytesRecv, LPWSAOVERLAPPED overlapped, DWORD flags);
+    void handleSongPkt(DWORD recvBytes);
+--
+-- DATE:    April 10th, 2017
+--
+-- DESIGNER: Brody McCrone
+--
+-- PROGRAMMER: Brody McCrone
+--
+-- NOTES: Functions for the receiving side of the song streamer.
+--
+------------------------------------------------------------------------------*/
 #include "songstreamreceiver.h"
 #include "utilities.h"
 #include "debugwindow.h"
 #include <QDebug>
 #include "audiomanager.h"
 
+/*------------------------------------------------------------------------------
+-- FUNCTION: SongStreamReceiver::SongStreamReceiver(int listenPort)
+--
+-- DATE:    April 10th, 2017
+--
+-- DESIGNER: Brody McCrone
+--
+-- PROGRAMMER: Brody McCrone
+--
+-- INTERFACE: SongStreamReceiver::SongStreamReceiver(int listenPort)
+--
+-- PARAMETERS: N/A
+--
+-- RETURNS: N/A
+--
+-- NOTES: Creates UDP socket and bind to the UDP socket
+--
+------------------------------------------------------------------------------*/
 SongStreamReceiver::SongStreamReceiver(int listenPort) {
     sock = createSocket(SOCK_DGRAM);
     bindSocket(sock, createAddress(htonl(INADDR_ANY), htons(listenPort)));
@@ -12,6 +50,24 @@ SongStreamReceiver::SongStreamReceiver(int listenPort) {
     gotPacket = CreateEvent(NULL, FALSE, FALSE, NULL);
 }
 
+/*------------------------------------------------------------------------------
+-- FUNCTION: void SongStreamReceiver::init()
+--
+-- DATE:    April 10th, 2017
+--
+-- DESIGNER: Brody McCrone
+--
+-- PROGRAMMER: Brody McCrone
+--
+-- INTERFACE: void SongStreamReceiver::init()
+--
+-- PARAMETERS: N/A
+--
+-- RETURNS: N/A
+--
+-- NOTES: Join multicast network and receive file from socket
+--
+------------------------------------------------------------------------------*/
 void SongStreamReceiver::init() {
     joinMulticast(sock);
     olapWrap.receiver = this;
@@ -21,6 +77,24 @@ void SongStreamReceiver::init() {
     }
 }
 
+/*------------------------------------------------------------------------------
+-- FUNCTION: void CALLBACK SongStreamReceiver::receiveSongStreamRoutine(DWORD err, DWORD bytesRecv, LPWSAOVERLAPPED overlapped, DWORD flags) {
+--
+-- DATE:    April 10th, 2017
+--
+-- DESIGNER: Brody McCrone
+--
+-- PROGRAMMER: Brody McCrone
+--
+-- INTERFACE: void CALLBACK SongStreamReceiver::receiveSongStreamRoutine(DWORD err, DWORD bytesRecv, LPWSAOVERLAPPED overlapped, DWORD flags) {
+--
+-- PARAMETERS: N/A
+--
+-- RETURNS: N/A
+--
+-- NOTES: Callback function for the completion routine to receive the song stream
+--
+------------------------------------------------------------------------------*/
 void CALLBACK SongStreamReceiver::receiveSongStreamRoutine(DWORD err, DWORD bytesRecv, LPWSAOVERLAPPED overlapped, DWORD flags) {
     if (err != 0) {
         DebugWindow::get()->logd(QString("receiveSongStreamRoutine failed, error code: ") + itoq(err));
@@ -31,6 +105,24 @@ void CALLBACK SongStreamReceiver::receiveSongStreamRoutine(DWORD err, DWORD byte
     reinterpret_cast<SongStreamReceiverOlapWrap *>(overlapped)->receiver->handleSongPkt(bytesRecv);
 }
 
+/*------------------------------------------------------------------------------
+-- FUNCTION: void SongStreamReceiver::handleSongPkt(DWORD recvBytes)
+--
+-- DATE:    April 10th, 2017
+--
+-- DESIGNER: Brody McCrone
+--
+-- PROGRAMMER: Brody McCrone
+--
+-- INTERFACE: void SongStreamReceiver::handleSongPkt(DWORD recvBytes)
+--
+-- PARAMETERS: N/A
+--
+-- RETURNS: N/A
+--
+-- NOTES: Handle the packet that was received
+--
+------------------------------------------------------------------------------*/
 void SongStreamReceiver::handleSongPkt(DWORD recvBytes) {
     char *tempBuffer = buffer;
     int offset = 0;
